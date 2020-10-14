b314f134dca709e5d0f2f61140eb2310c6da4d39
Matt Reeds <mattlreed@gmail.com>
2020-10-14 09:56:00 -0500

    intro console interaction

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Intro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! I'm glad to meet you.");
+            Console.WriteLine($"How are you doing today, {name}?");
+            string mood = Console.ReadLine();
+            Console.WriteLine($"{mood}? Your day can only get better from hear!");
+        }
+    }
+}
